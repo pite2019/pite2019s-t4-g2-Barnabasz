3936de9c946df04a1335a1d8cc9b4c3e7d57c84b
Kamil Halat <6halat@stud215.ftj.agh.edu.pl>
2019-04-12 14:31:14 +0200

    update

--- task.py
+++ task.py
@@ -24,16 +24,14 @@
 #Delete these comments before commit!
 #Good luck.
 
-import matrix
+from matrix import Matrix 
 
 matrix_1 = Matrix(4,5,6,7)
-matrix_2 = Matrix(2,2,2,1)
+	matrix_2 = Matrix(2,2,2,1)
 
-
-
-matrix_3 = matrix_2.add(matrix_1)
-matrix_4 = matrix_3.prod(matrix_2)
-print(matrix_3)
-print(matrix_4)
-matrix_1[0][1] = 23
-print(matrix_1)
+	matrix_3 = matrix_2.add(matrix_1)
+	matrix_4 = matrix_3.prod(matrix_2)
+	print(matrix_3)
+	print(matrix_4)
+	matrix_1[0][1] = 23
+	print(matrix_1)
